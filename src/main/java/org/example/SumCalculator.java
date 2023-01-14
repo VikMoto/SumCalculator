f18ce2f493d1a0cf8277bd45ebf2f1364e40afbc
VikMoto <ig.basalt@gmail.com>
2023-01-15 00:34:39 +0200

    SumCalculator 15.01.2023

---
+++ src/main/java/org/example/SumCalculator.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class SumCalculator {
+    public static int sum(int n) {
+        int result = 0;
+        if (n == 0) {
+            throw new IllegalArgumentException("Invalid input argument : " + n);
+        } else {
+
+            for (int i = 1; i <= n; i++) {
+                result += i;
+//            System.out.println("result = " + result);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/org/example/SumCalculatorTest.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SumCalculatorTest {
+    private SumCalculator sumCalculator;
+
+    @BeforeEach
+    public void beforeEach() {
+        sumCalculator = new SumCalculator();
+    }
+    @Test
+    public void testSum1 () {
+        int actual = sumCalculator.sum(1);
+        int expected = 1;
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void testSum3 () {
+        int actual = sumCalculator.sum(3);
+        int expected = 6;
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void testSum6 () {
+        int actual = sumCalculator.sum(6);
+        int expected = 21;
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void testSum10 () {
+        int actual = sumCalculator.sum(10);
+        int expected = 55;
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void testSum56 () {
+        int actual = sumCalculator.sum(21);
+        int expected = 231;
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testIllegalArgumentException0 () {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> sumCalculator.sum(0));
+    }
+
+
+
+}
